5d43bcc4d0a14cef2056d24a648cbb2f4d2d9e0d
16999 <faisonslepoint@protonmail.com>
2021-09-29 09:19:39 +0200

    Création d'une fenêtre

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-
-// Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
+#include <state.h>
 
 void testSFML() {
     sf::Texture texture;
 }
 
-// Fin test SFML
-
-#include <state.h>
 
 using namespace std;
 using namespace state;
@@ -19,7 +15,18 @@ int main(int argc,char* argv[])
     Exemple exemple;
     exemple.setX(53);
 
-    cout << "It works ! TEST EDDY222" << endl;
-
+    cout << "Lancement du jeu" << endl;
+    sf::Window window(sf::VideoMode(800, 600), "Fenetre test");
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
     return 0;
 }
