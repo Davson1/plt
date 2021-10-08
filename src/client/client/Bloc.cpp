74c88581c76ad5f7503f133120088ebf768b952c
16999 <faisonslepoint@protonmail.com>
2021-10-08 10:44:54 +0200

    ajout fonctions.h

--- src/client/client/Bloc.cpp
+++ src/client/client/Bloc.cpp
@@ -1,25 +1,33 @@
 #include "Bloc.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
+namespace client {
 
-void client::setSprite (int sprite)
-{
-  /*switch(sprite)
+  void Bloc::setSprite (int sprite)
   {
-    case 0 : this->imagePath = "../res/tile_0073.png";
-    case 1 : this->imagePath = "../res/tile_0033.png";
-    case 3 : this->imagePath = "../res/tile_0021.png";
-    case 4 : this->imagePath = "../res/tile_0121.png";
-    case 5 : this->imagePath = "../res/tile_0122.png";
-    case 6 : this->imagePath = "../res/tile_0123.png";
-    case 7 : this->imagePath = "../res/tile_0021.png";
-    case 8 : this->imagePath = "../res/tile_0022.png";
-    case 9 : this->imagePath = "../res/tile_0023.png";
-    default : this->imagePath = "";
-  }*/
-}
+    switch(sprite)
+    {
+      case 1 : this->imagePath = "../res/tile_0073.png";
+      case 2 : this->imagePath = "../res/tile_0033.png";
+      case 3 : this->imagePath = "../res/tile_0021.png";
+      case 4 : this->imagePath = "../res/tile_0121.png";
+      case 5 : this->imagePath = "../res/tile_0122.png";
+      case 6 : this->imagePath = "../res/tile_0123.png";
+      case 7 : this->imagePath = "../res/tile_0021.png";
+      case 8 : this->imagePath = "../res/tile_0022.png";
+      case 9 : this->imagePath = "../res/tile_0023.png";
+      default : this->imagePath = "";
+    }
+  }
 
-void client::drawSprite (int x, int y)
-{
-
+  void Bloc::drawSprite (int x, int y)
+  {
+    sf::Texture oneBlocTexture;
+    sf::Sprite oneBloc;
+    oneBlocTexture.loadFromFile("../res/tile_0022.png");
+    oneBloc.setTexture(oneBlocTexture);
+    oneBloc.setPosition(x,y);
+  }
 
 }
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -3,12 +3,10 @@
 
 
 
-
+//client::Bloc b;
 
 int main(int argc,char* argv[])
 {
-  client::MyClass myObj;     // Create an object of MyClass
-myObj.myMethod();  // Call the method
   refresh();
   return 0;
 }
